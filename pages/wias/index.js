aef17e216a7bc7342c3a004a758f8681f195a43a
Alejandro Upegui <aub.17@hotmail.com>
2020-08-09 22:04:37 -0500

    Starting wth Login

--- pages/wias/index.js
+++ pages/wias/index.js
@@ -1,17 +1,10 @@
 import Link from "next/link";
-import AppLayout from "../../components/AppLayout/index"
-export default function Wias({userName}) {
+
+export default function Wias(params) {
     return (
     <>
-    <AppLayout>
-        <h1>This a timeLine of {userName}</h1>
-        <Link href = "/"><a>Go Home</a></Link>
-    </AppLayout>
+    <h1>This a timeLine</h1>
+    <Link href = "/"><a>Go Home</a></Link>
     </>
     )
-
-}
-Wias.getInitialProps =  ()=>{
-    return fetch("http://localhost:3000/api/hello")
-    .then((data)=>data.json())
 }
